9e729d02bdadcaaa141f389b204b160075de503d
Teun van Brakel <teunvbrakel@gmail.com>
2021-10-15 15:00:08 +0200

    added driver model

--- Back-end/f1-information-backend/f1-information-backend/Models/Driver.cs
+++ Back-end/f1-information-backend/f1-information-backend/Models/Driver.cs
@@ -7,5 +7,16 @@ namespace f1_information_backend.Models
 {
     public class Driver
     {
+        public int Id { get; set; }
+        private string name;
+        private int age;
+        private string currentTeam;
+
+        public Driver(string _name, int _age, string _currentTeam)
+        {
+            name = _name;
+            age = _age;
+            currentTeam = _currentTeam;
+        }
     }
 }
