9e8b88c9a0fdfb9733f7cf7db107704e8e6e7f21
Teun van Brakel <teunvbrakel@gmail.com>
2021-10-15 15:02:49 +0200

    frontend can sent request to backend.

--- Front-end/f1-information-frontend/src/app/app.component.ts
+++ Front-end/f1-information-frontend/src/app/app.component.ts
@@ -1,4 +1,9 @@
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
 import { Component } from '@angular/core';
+import { observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+var data = undefined;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'f1-information-frontend';
+  
+  constructor(private http: HttpClient){
+    data = this.GetDrivers();
+    // console.log( data);
+  }
+  public GetDrivers() {
+    var test = this.http.get('http://localhost:5000/weatherforecast').subscribe(value => data = value)
+    console.log(this.http.get('http://localhost:5000/weatherforecast').subscribe(value => value.toString()));
+    return test;
+  }
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 }
+
